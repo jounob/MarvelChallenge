2f0d6c795457e9741773b12df8a8e50bcb45961d
jounob <obeiwiljephnie@gmail.com>
2022-04-14 01:51:22 -0300

    Implementation navigation when icon is selected.

--- app/src/main/java/com/esther/intermediachallenge/ui/main/MainActivity.kt
+++ app/src/main/java/com/esther/intermediachallenge/ui/main/MainActivity.kt
@@ -2,11 +2,26 @@ package com.esther.intermediachallenge.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.esther.intermediachallenge.R
+import com.esther.intermediachallenge.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnNavView.itemIconTintList = null
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+        val bottomNavigationView = binding.btnNavView
+        setupWithNavController(bottomNavigationView, navController)
     }
 }
